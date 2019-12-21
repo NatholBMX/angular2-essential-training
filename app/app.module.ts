f699a67d530a8e2e008d707f484e59f7cd7407d8
nathol <nikolaj.waller@campus.tu-berlin.de>
2019-12-21 09:13:56 +0100

    Finished lesson 4

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { MediaItemComponent } from './media-item.component';
-import { MediaItemListComponent } from './media-item-list.component';
-import { FavoriteDirective } from './favorite.directive';
-import { CategoryListPipe } from './category-list.pipe';
-import { MediaItemFormComponent } from './media-item-form.component';
+import { AppComponent } from "./app.component";
+import { MediaItemComponent } from "./media-item.component";
+import { MediaItemListComponent } from "./media-item-list.component";
+import { FavoriteDirective } from "./favorite.directive";
+import { CategoryListPipe } from "./category-list.pipe";
+import { MediaItemFormComponent } from "./media-item-form.component";
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, ReactiveFormsModule],
   declarations: [
     AppComponent,
     MediaItemComponent,
@@ -20,8 +19,6 @@ import { MediaItemFormComponent } from './media-item-form.component';
     CategoryListPipe,
     MediaItemFormComponent
   ],
-  bootstrap: [
-    AppComponent
-  ]
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
